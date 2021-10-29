405dad95dd4abee8093931d111a746b6ad1727f6
saad-chohan <86551822+saad-chohan@users.noreply.github.com>
2021-10-29 22:37:07 +0500

    add all files

--- server.mjs
+++ server.mjs
@@ -1,20 +1,20 @@
 import express from 'express';
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use((req,res,next)=>{
     console.log("a request came", Date.now());
     next()
 })
 
+app.get('/', (req, res) => {
+  res.send('Hi I am a hello world Server program')
+})
 app.get('/profile', (req, res) => {
   res.send('here is your profile')
 })
 app.get('/home', (req, res) => {
     res.send('here is your home')
-})
-app.get('/', (req, res) => {
-    res.send('Hi I am a hello world Server program')
 })
 
 app.listen(port, () => {
